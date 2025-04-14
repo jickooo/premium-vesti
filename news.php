2f1a7df4358071d99308b88d0776e15ca6cb4a94
jickooo <62907312+jickooo@users.noreply.github.com>
2025-04-14 15:12:50 +0200

    Add files via upload

--- news.php
+++ news.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 $apiKey = 'YOUR_NEWS_API_KEY';
-$apiUrl = 'https://newsdata.io/api/1/news?apikey=' . $apiKey . '&q=mobile%20phones&language=sr';
+$apiUrl = 'https://newsdata.io/api/1/news?apikey=pub_80387fb505b3f83d87f6f1d65b65caa8ecdd7&q=mobilni%20telefoni';
 
 $response = file_get_contents($apiUrl);
 echo $response;
